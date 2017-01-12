054d8fc93850568ddd702c6378de2d3278b38d1f
Julie Allinson <j.allinson@gmail.com>
2017-01-12 11:09:15 +0000

    added journal article model

---
+++ wrro-eprint-100182.js
@@ -0,0 +1,133 @@
+    {
+      "eprintid": 100182,
+      "dates": [
+        {
+          "date_type": "published",
+          "date": "2016-05"
+        },
+        {
+          "date_type": "accepted",
+          "date": "2014-11-04"
+        },
+        {
+          "date_type": "published_online",
+          "date": "2016-01-09"
+        }
+      ],
+      "date": "2016-05",
+      "documents": [
+          {
+            "placement": 1,
+            "eprintid": 100182,
+            "files": [
+                {
+                  "hash_type": "MD5",
+                  "mtime": "2016-05-25 16:25:04",
+                  "datasetid": "document",
+                  "fileid": 1050920,
+                  "objectid": 551673,
+                  "uri": "http:\/\/eprints.whiterose.ac.uk\/id\/file\/1050920",
+                  "mime_type": "application\/pdf",
+                  "hash": "b6e3bc065d0a9a2c34cd8e72af53134d",
+                  "filesize": 224791,
+                  "filename": "hisr12127.pdf"
+                }
+            ],
+            "content": "published",
+            "rev_number": 3,
+            "pos": 1,
+            "language": "en",
+            "license": "cc_by",
+            "mime_type": "application\/pdf",
+            "main": "hisr12127.pdf",
+            "uri": "http:\/\/eprints.whiterose.ac.uk\/id\/document\/551673",
+            "coversheetid": 2,
+            "format": "text",
+            "docid": 551673,
+            "security": "public",
+            "formatdesc": "hisr12127"
+          }
+      ],
+      "official_url": "http:\/\/dx.doi.org\/10.1111\/1468-2281.12127",
+      "pure_status": "approved",
+      "rioxx2_free_to_read": {
+        "free_to_read": "Yes"
+      },
+      "rioxx2_title": "A matter of trust: the royal regulation of England's French residents during wartime, 1294-1377",
+      "pagerange": "208-226",
+      "hoa_date_foa": "2016-11-06",
+      "date_type": "published",
+      "rioxx2_dateAccepted": "2014-11-04",
+      "rioxx2_identifier": "http:\/\/eprints.whiterose.ac.uk\/100182\/1\/hisr12127.pdf",
+      "hoa_compliant": 500,
+      "datestamp": "2016-05-25 16:25:03",
+      "uri": "http:\/\/eprints.whiterose.ac.uk\/id\/eprint\/100182",
+      "rioxx2_author": [
+        {
+          "author": "Lambert, Bart Roger Denise"
+        },
+        {
+          "id": "http:\/\/orcid.org\/0000-0003-0044-726X",
+          "author": "Ormrod, William Mark"
+        }
+      ],
+      "rioxx2_publication_date": "2016-05",
+      "publication": "Historical Research",
+      "type": "article",
+      "title": "A matter of trust: the royal regulation of England's French residents during wartime, 1294-1377",
+      "rioxx2_version": "VoR",
+      "pages": 19,
+      "source": "pure",
+      "hoa_date_fcd": "2016-11-06",
+      "userid": 2847,
+      "series_number": 244,
+      "pureid": 48784616,
+      "issn": "0950-3471",
+      "rev_number": 20,
+      "dir": "disk0\/00\/10\/01\/82",
+      "creators": [
+        {
+          "name": {
+            "lineage": null,
+            "given": "Bart Roger Denise",
+            "honourific": null,
+            "family": "Lambert"
+          },
+          "id": "bart.lambert@york.ac.uk"
+        },
+        {
+          "name": {
+            "lineage": null,
+            "given": "William Mark",
+            "honourific": null,
+            "family": "Ormrod"
+          },
+          "id": "0000-0003-0044-726X"
+        }
+      ],
+      "rioxx2_format": "application\/pdf",
+      "lastmod": "2016-11-25 05:41:55",
+      "ispublished": "pub",
+      "article_type": "article",
+      "rioxx2_source": "0950-3471",
+      "metadata_visibility": "show",
+      "eprint_status": "archive",
+      "hoa_version_fcd": "VoR",
+      "status_changed": "2016-05-25 16:25:03",
+      "rioxx2_language": "en",
+      "rioxx2_license_ref": {
+        "license_ref": "http:\/\/creativecommons.org\/licenses\/by\/2.5",
+        "start_date": "2016-05"
+      },
+      "series_volume": 89,
+      "hoa_date_pub": "2016-01-09",
+      "note": "© 2016, The Authors Historical Research published by John Wiley & Sons Ltd on behalf of Institute of Historical Research.",
+      "full_text_status": "public",
+      "rioxx2_type": "Journal Article\/Review",
+      "refereed": "TRUE",
+      "contact_email": "bart.lambert@york.ac.uk",
+      "hoa_date_acc": "2014-11-04",
+      "institution": [
+        "York"
+      ]
+    }
